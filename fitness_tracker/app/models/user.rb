15bf81de96c790e8c7bec5d8934848a9b893a031
kanya01 <kanyamosesm@gmail.com>
2023-06-05 22:24:47 +0100

    devise authentication

--- fitness_tracker/app/models/user.rb
+++ fitness_tracker/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :activities
   has_many :nutritions
   has_many :goals
